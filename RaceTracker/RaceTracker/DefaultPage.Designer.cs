012d01cd596ce95941d06e85a994e3dca9dc2891
iRobotNinja <mmassrobots3@gmail.com>
2018-10-17 00:00:41 -0400

    Stopwatch working

--- RaceTracker/RaceTracker/DefaultPage.Designer.cs
+++ RaceTracker/RaceTracker/DefaultPage.Designer.cs
@@ -31,19 +31,115 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.RaceTime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TimeRemainBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.debugSave = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(12, 76);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(96, 76);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Visible = false;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // RaceTime
+            // 
+            this.RaceTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RaceTime.Location = new System.Drawing.Point(12, 26);
+            this.RaceTime.Name = "RaceTime";
+            this.RaceTime.Size = new System.Drawing.Size(159, 40);
+            this.RaceTime.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Total Race Time\r\n";
+            // 
+            // TimeRemainBox
+            // 
+            this.TimeRemainBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimeRemainBox.Location = new System.Drawing.Point(197, 26);
+            this.TimeRemainBox.Name = "TimeRemainBox";
+            this.TimeRemainBox.Size = new System.Drawing.Size(159, 40);
+            this.TimeRemainBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(194, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Time Remaining";
+            // 
+            // debugSave
+            // 
+            this.debugSave.Location = new System.Drawing.Point(698, 407);
+            this.debugSave.Name = "debugSave";
+            this.debugSave.Size = new System.Drawing.Size(75, 23);
+            this.debugSave.TabIndex = 7;
+            this.debugSave.Text = "Save to file";
+            this.debugSave.UseVisualStyleBackColor = true;
+            this.debugSave.Click += new System.EventHandler(this.debugSave_Click);
             // 
             // DefaultPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.debugSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TimeRemainBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RaceTime);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
             this.Name = "DefaultPage";
             this.Text = "RIOT Racing Race Timer";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.TextBox RaceTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TimeRemainBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button debugSave;
     }
 }
--- RaceTracker/RaceTracker/DefaultPage.cs
+++ RaceTracker/RaceTracker/DefaultPage.cs
@@ -2,19 +2,82 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
+
 namespace RaceTracker
 {
     public partial class DefaultPage : Form
     {
+
+        private const int timerInterval = 10;
+        private Stopwatch stopWatch = new Stopwatch();
+        private DataTable data = new DataTable(); 
+
+        // test setup datatable
+        
+
         public DefaultPage()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            RaceTime.Text = String.Format("{0}:{1:D2}:{2:D2}:{3:D2}", 
+                (int)stopWatch.Elapsed.TotalHours, 
+                stopWatch.Elapsed.Minutes, 
+                stopWatch.Elapsed.Seconds, 
+                stopWatch.Elapsed.Milliseconds);
+
+            RaceTime.Text = String.Format("{0}:{1:D2}:{2:D2}:{3:D2}",
+                (int)stopWatch.Elapsed.TotalHours,
+                stopWatch.Elapsed.Minutes,
+                stopWatch.Elapsed.Seconds,
+                stopWatch.Elapsed.Milliseconds);
+
+
+        }
+
+        // start button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!stopWatch.IsRunning)
+            {
+                timer1.Start();
+                timer1.Interval = timerInterval;
+                stopWatch.Start();
+                StopButton.Visible = true;
+            }
+        }
+        
+        // stop button
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            if(stopWatch.IsRunning)
+                stopWatch.Stop();
+        }
+
+        private void debugSave_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // test setup data table
+
+
+            data.Columns.Add("column1");
+            data.Rows.Add("row1");
+
+
+
+            string[] colNames = 
         }
     }
 }
